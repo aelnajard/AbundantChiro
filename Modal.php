b1477ee5d46fc5372d0ecb8ec42cb058a44367d7
Your Name <you@example.com>
2021-01-28 22:44:12 -0500

    Modal

---
+++ Modal.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Abundant Chiropractic </title>
+
+    <link rel="stylesheet" type="text/css" href="CSS/Chiro.css">
+      <link rel="stylesheet" type="text/css" href="CSS/Modal.css">
+  </head>
+
+  <body>
+
+
+    <div class="Modal-Container">
+      <div class="Mod-Left">
+
+      </div>
+
+      <div class="Mod-Right">
+
+          <h1> Abundant </br> Chiropractic </h1>
+          <h2> Welcomes You </h2>
+
+          <div class="ModPic">
+
+          </div>
+          <p> Alex Rubin <p>
+          <p> Chiropractic Doctor </p>
+
+          <h3> 610.630.0123 </h3>
+          <p> <span class="ModSpan"> Please call or text us </br>
+          for the fastest response </span> </p>
+
+      </div>
+    </div>
+
+
+  </body>
+</html>
